ab2b92cf426c1c034a6f15509b164088224d91b1
Trần Sĩ  Tiến <106511449+TranSiTien@users.noreply.github.com>
2022-12-02 06:18:22 +0700

    Update rating_coment.php

--- view/partials/rating_coment.php
+++ view/partials/rating_coment.php
@@ -9,70 +9,73 @@ if (!$rat_cmt) {
     echo "<h2>Không có đánh giá và bình luận</h2>";
 } else {
 
-    foreach ($rat_cmt as $each)
+    foreach ($rat_cmt as $each) {
+
 ?>
 
-    <div class="sumary">
-        <div class="parent_info">
-            <!-- comment header -->
-            <div class="comment_header">
-                <div class="name">
-                    <?= $each['name'] ?>
+        <div class="sumary">
+            <div class="parent_info">
+                <!-- comment header -->
+                <div class="comment_header">
+                    <div class="name">
+                        <?= $each['name'] ?>
+                    </div>
+                    <div class="time">
+                        <?= $each['create_at'] ?>
+                    </div>
+                    <div class="rating">
+                        <?= $each['rating'] ?>
+                    </div>
                 </div>
-                <div class="time">
-                    <?= $each['create_at'] ?>
-                </div>
-                <div class="rating">
-                    <?= $each['rating'] ?>
-                </div>
-            </div>
 
-            <!-- comment content -->
-            <div class="comment_content">
-                <p>
-                    <?= $each['comment'] ?>
-                </p>
-            </div>
-
-            <!-- comment footer -->
-            <div class="comment_footer">
-                <div class="like btn">
-                    <span>Thích</span>
+                <!-- comment content -->
+                <div class="comment_content">
+                    <p>
+                        <?= $each['comment'] ?>
+                    </p>
                 </div>
-                <div class="reply btn">
-                    <span>Trả lời</span>
-                </div>
-            </div>
 
-            <?php
-            $reply_id = $each['id'];
-            $sql = "select rc.comment, cus.name, rc.create_at from rating_comment as rc 
+                <!-- comment footer -->
+                <div class="comment_footer">
+                    <div class="like btn">
+                        <span>Thích</span>
+                    </div>
+                    <div class="reply btn">
+                        <span>Trả lời</span>
+                    </div>
+                </div>
+
+                <?php
+                $reply_id = $each['id'];
+                $sql = "select rc.comment, cus.name, rc.create_at from rating_comment as rc 
         join rating_comment as rc2 on rc.reply_id = rc2.id
         join customers as cus on rc.customer_id = cus.id
         where rc.reply_id = $reply_id";
-            $reply = $connect_DB->execute_sql($sql);
+                $reply = $connect_DB->execute_sql($sql);
 
-            foreach ($reply as $each_reply) {
-            ?>
-                <br>
-                <!-- comment reply -->
-                <div class="comment_reply">
-                    <div class="comment_reply_header">
-                        <div class="name">
-                            <?= $each_reply['name'] ?>
+                foreach ($reply as $each_reply) {
+                ?>
+                    <br>
+                    <!-- comment reply -->
+                    <div class="comment_reply">
+                        <div class="comment_reply_header">
+                            <div class="name">
+                                <?= $each_reply['name'] ?>
+                            </div>
+                            <div class="time">
+                                <?= $each_reply['create_at'] ?>
+                            </div>
                         </div>
-                        <div class="time">
-                            <?= $each_reply['create_at'] ?>
+                        <div class="comment_reply_content">
+                            <p>
+                                <?= $each_reply['comment'] ?>
+                            </p>
                         </div>
                     </div>
-                    <div class="comment_reply_content">
-                        <p>
-                            <?= $each_reply['comment'] ?>
-                        </p>
-                    </div>
-                </div>
-            <?php } ?>
-        </div>
+                <?php } ?>
+            </div>
 
-    <?
+    <?php
+    }
 }
+    ?>
